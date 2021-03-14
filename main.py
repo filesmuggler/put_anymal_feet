e73e25d112be83b5212dad25a0fb7a1e2e4ea91e
filesmuggler <krzste09@outlook.com>
2021-03-14 17:53:15 +0100

    x

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.decomposition import PCA
 import re
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from scipy.fft import fft, fftfreq
+from scipy import signal
 
-PATH = './logs/log.csv'
+PATH = './logs/1203/log.csv'
 classes_names = [('_cup', 0), ('_bottle', 1), ('_apple', 2)]
 
 def plot_experiment(forces,acc,gyro, quat):
@@ -19,12 +20,50 @@ def plot_experiment(forces,acc,gyro, quat):
     axes[3].plot(quat[:-1000])
     plt.show()
 
-def get_pca(data):
-    print("xd")
-    pca = PCA(n_components=2)
+def process_data(data):
+    Fs = 400
+    M = 1024
+    dx = data['x'].to_numpy()
+    dy = data['y'].to_numpy()
+    dz = data['z'].to_numpy()
+    dxf = fft(dx)
+    nxf = fftfreq(len(dx),1/Fs)
+    plt.stem(nxf, np.abs(dxf), use_line_collection=True)
+    plt.grid()
+    plt.show()
 
+    freqs, times, Sx = signal.spectrogram(dx, fs=Fs)
 
-def process_experiment(forces, acc, gyro, poke_point):
+    f, ax = plt.subplots(figsize=(4.8, 2.4))
+    ax.pcolormesh(times, freqs, Sx, cmap='viridis')
+    ax.set_ylabel('Frequency [Hz]')
+    ax.set_xlabel('Time [s]');
+    print("xdd")
+    plt.plot()
+    plt.show()
+
+def process_poke_point(pokepoint):
+    pokes = re.findall(r'\d+\.\d+', pokepoint)
+    for index, item in enumerate(pokes):
+        pokes[index] = float(item)
+    return pokes
+
+def process_quat(quat):
+    pass
+
+def analyze_data(forces=None,acc=None,gyro=None,pokepoint=None,quat=None):
+    if forces is not None:
+        process_data(forces)
+    # if acc is not None:
+    #     process_data(acc)
+    # if gyro is not None:
+    #     process_data(gyro)
+    # if pokepoint is not None:
+    #     process_poke_point(pokepoint)
+    # if quat is not None:
+    #     process_quat(quat)
+
+def old_analyze_data(forces, acc, gyro, poke_point):
     pokes = re.findall(r'\d+\.\d+', poke_point)
     for index, item in enumerate(pokes):
         pokes[index] = float(item)
@@ -40,9 +79,9 @@ def read_experiment(row_path):
         if obj_class[0] == name:
             obj_id = label
 
-    forces = pd.read_csv(os.path.join("logs",row_path['forces_path'].split("/")[-1]), index_col=0)
-    acc = pd.read_csv(os.path.join("logs",row_path['acc_path'].split("/")[-1]), index_col=0)
-    gyro = pd.read_csv(os.path.join("logs",row_path['gyro_path'].split("/")[-1]), index_col=0)
+    forces = pd.read_csv(os.path.join("logs/1203",row_path['forces_path'].split("/")[-1]), index_col=0)
+    acc = pd.read_csv(os.path.join("logs/1203",row_path['acc_path'].split("/")[-1]), index_col=0)
+    gyro = pd.read_csv(os.path.join("logs/1203",row_path['gyro_path'].split("/")[-1]), index_col=0)
     poke_point = row_path["poke_point"]
     return forces,acc,gyro,poke_point,obj_id
 
@@ -54,14 +93,15 @@ def main():
     number_of_experiments = len(mylog)
     for i in range(number_of_experiments):
         forces_raw,acc_raw,gyro_raw,poke_point_raw, obj_id = read_experiment(mylog.loc[i,:])
-        forces_p, acc_p, gyro_p, pokes_p = process_experiment(forces_raw, acc_raw, gyro_raw, poke_point_raw)
+        forces_p, acc_p, gyro_p, pokes_p = old_analyze_data(forces_raw, acc_raw, gyro_raw, poke_point_raw)
+        #analyze_data(forces_raw, acc_raw, gyro_raw, poke_point_raw)
         a_row = pd.Series([forces_p[0],forces_p[1],forces_p[2],acc_p[0],acc_p[1],acc_p[2],gyro_p[0],gyro_p[1],gyro_p[2],pokes_p[0],pokes_p[1],pokes_p[2],obj_id],index=cl)
         row_df = pd.DataFrame([a_row])
         combined_data = pd.concat([row_df,combined_data],ignore_index=True)
 
 
     cm_data = combined_data.loc[:,combined_data.columns !='class']
-
+    #
     pca = PCA(n_components=2)
     pca.fit(cm_data)
     cm_data = pca.transform(cm_data)
---
+++ main2.py
@@ -0,0 +1,6 @@
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.decomposition import PCA
+import re
+
